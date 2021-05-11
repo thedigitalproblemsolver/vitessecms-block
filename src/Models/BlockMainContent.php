e1fcf96b40b1269f4bdd41b14cbdc1817c6b2976
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-05-11 21:16:11 +0200

    Added useDatagroupTemplate to BlockMainContent.php

--- src/Models/BlockMainContent.php
+++ src/Models/BlockMainContent.php
@@ -12,9 +12,11 @@ class BlockMainContent extends AbstractBlockModel
 
         if ($this->view->hasCurrentItem()) :
             $item = $this->view->getCurrentItem();
-            $datagroup = $this->di->repositories->datagroup->getById($item->getDatagroup());
-            if ($datagroup->getTemplate() !== null) :
-                $this->template = $datagroup->getTemplate();
+            if($block->getBool('useDatagroupTemplate')) :
+                $datagroup = $this->di->repositories->datagroup->getById($item->getDatagroup());
+                if ($datagroup->getTemplate() !== null) :
+                    $this->template = $datagroup->getTemplate();
+                endif;
             endif;
             $block->set('imageFullWidth', true);
             $this->di->eventsManager->fire(get_class($this) . ':parse', $this, $block);
