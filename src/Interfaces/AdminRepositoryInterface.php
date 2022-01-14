abadbfa894d3123f0452aafd71671c9ce558f3b1
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2022-01-14 14:55:42 +0100

    Fixed Interface mismatch

--- src/Interfaces/AdminRepositoryInterface.php
+++ src/Interfaces/AdminRepositoryInterface.php
@@ -4,6 +4,7 @@ namespace VitesseCms\Block\Interfaces;
 
 use VitesseCms\Block\Repositories\BlockPositionRepository;
 use VitesseCms\Block\Repositories\BlockRepository;
+use VitesseCms\Content\Repositories\ItemRepository;
 use VitesseCms\Datagroup\Repositories\DatagroupRepository;
 use VitesseCms\Database\Interfaces\BaseRepositoriesInterface;
 use VitesseCms\Mustache\Repositories\LayoutRepository;
@@ -13,6 +14,7 @@ use VitesseCms\Mustache\Repositories\LayoutRepository;
  * @property BlockRepository $block
  * @property DatagroupRepository $datagroup
  * @property LayoutRepository $layout
+ * @property ItemRepository $item
  */
 interface AdminRepositoryInterface extends BaseRepositoriesInterface
 {
