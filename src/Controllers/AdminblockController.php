247ff1ea5b3ec1248b995a02027a2c4bc211018b
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-07-06 10:37:30 +0200

    Refactores to new admin Controller

--- src/Controllers/AdminblockController.php
+++ src/Controllers/AdminblockController.php
@@ -2,18 +2,79 @@
 
 namespace VitesseCms\Block\Controllers;
 
-use VitesseCms\Admin\AbstractAdminEventController;
+use VitesseCms\Admin\Interfaces\AdminModelAddableInterface;
+use VitesseCms\Admin\Interfaces\AdminModelDeletableInterface;
+use VitesseCms\Admin\Interfaces\AdminModelEditableInterface;
+use VitesseCms\Admin\Interfaces\AdminModelFormInterface;
+use VitesseCms\Admin\Interfaces\AdminModelListInterface;
+use VitesseCms\Admin\Interfaces\AdminModelPublishableInterface;
+use VitesseCms\Admin\Interfaces\AdminModelSaveInterface;
+use VitesseCms\Admin\Traits\TraitAdminModelAddable;
+use VitesseCms\Admin\Traits\TraitAdminModelDeletable;
+use VitesseCms\Admin\Traits\TraitAdminModelEditable;
+use VitesseCms\Admin\Traits\TraitAdminModelList;
+use VitesseCms\Admin\Traits\TraitAdminModelPublishable;
+use VitesseCms\Admin\Traits\TraitAdminModelSave;
+use VitesseCms\Block\Enum\BlockEnum;
 use VitesseCms\Block\Forms\BlockForm;
-use VitesseCms\Block\Forms\NewBlockForm;
-use VitesseCms\Block\Interfaces\AdminRepositoryInterface;
-use VitesseCms\Block\Interfaces\RepositoriesInterface;
 use VitesseCms\Block\Models\Block;
+use VitesseCms\Block\Repositories\BlockRepository;
+use VitesseCms\Core\AbstractControllerAdmin;
 use VitesseCms\Database\AbstractCollection;
-use VitesseCms\Form\AbstractForm;
+use VitesseCms\Database\Models\FindOrder;
+use VitesseCms\Database\Models\FindOrderIterator;
+use VitesseCms\Database\Models\FindValueIterator;
 
-class AdminblockController extends AbstractAdminEventController implements AdminRepositoryInterface
+class AdminblockController extends AbstractControllerAdmin implements
+    AdminModelPublishableInterface,
+    AdminModelListInterface,
+    AdminModelEditableInterface,
+    AdminModelSaveInterface,
+    AdminModelDeletableInterface,
+    AdminModelAddableInterface
 {
-    public function onConstruct()
+    use TraitAdminModelPublishable,
+        TraitAdminModelList,
+        TraitAdminModelEditable,
+        TraitAdminModelSave,
+        TraitAdminModelDeletable,
+        TraitAdminModelAddable
+        ;
+
+
+    private readonly BlockRepository $blockRepository;
+
+    public function OnConstruct()
+    {
+        parent::OnConstruct();
+
+        $this->blockRepository = $this->eventsManager->fire(BlockEnum::LISTENER_GET_REPOSITORY->value,new \stdClass());
+    }
+
+    public function getModel(string $id): ?AbstractCollection
+    {
+        return match ($id) {
+            'new' => new Block(),
+            default => $this->blockRepository->getById($id, false)
+        };
+    }
+
+    public function getModelList( ?FindValueIterator $findValueIterator): \ArrayIterator
+    {
+        return $this->blockRepository->findAll(
+            $findValueIterator,
+            false,
+            99999,
+            new FindOrderIterator([new FindOrder('createdAt', -1)])
+        );
+    }
+
+    public function getModelForm(): AdminModelFormInterface
+    {
+        return new BlockForm();
+    }
+
+    /*public function onConstruct()
     {
         parent::onConstruct();
 
@@ -35,9 +96,12 @@ class AdminblockController extends AbstractAdminEventController implements Admin
                 $this->repositories->block->getById($itemId, false)
             ));
         endif;
-    }
+    }*/
 
-    public function saveAction(
+    /**
+     * verwerken
+     */
+    /*public function saveAction(
         ?string $itemId = null,
         AbstractCollection $block = null,
         AbstractForm $form = null
@@ -55,5 +119,5 @@ class AdminblockController extends AbstractAdminEventController implements Admin
                 );
             endif;
         endif;
-    }
+    }*/
 }
