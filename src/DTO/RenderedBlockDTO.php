c3ff96af4501242da02879659595bc8bd8ffdc4a
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-07-27 15:41:21 +0200

    Add afterRenderBlock event

---
+++ src/DTO/RenderedBlockDTO.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\Block\DTO;
+
+use VitesseCms\Block\Models\Block;
+
+final class RenderedBlockDTO
+{
+    public function __construct(
+        public readonly Block $blockModel,
+        public readonly string $renderedBlock
+    )
+    {
+    }
+}
